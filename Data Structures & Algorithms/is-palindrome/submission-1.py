5d0a0efc92b608e0a3c6ad08b282b7023cd691b2
Abdul Amaan <110830860+Amaan1234567@users.noreply.github.com>
2026-08-28 21:55:01 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        formated=""
+        for i in range(len(s)):
+            if s[i].isalnum():
+                formated+=s[i]
+        # print(formated[-1::-1])
+        if formated == formated[-1::-1]:
+            return True
+        else:
+            return False
