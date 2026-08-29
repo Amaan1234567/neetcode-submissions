e291f382143d06d9155d133c2ecce0d36066403f
Abdul Amaan <110830860+Amaan1234567@users.noreply.github.com>
2026-08-29 11:44:27 +0530

    Add: two-integer-sum-ii - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-6.cpp
@@ -0,0 +1,26 @@
+#pragma optimise("O3","unroll-loops");
+
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        for (int i = 0; i < numbers.size()-1; i++) {
+            int l = i+1;
+            int r = numbers.size() - 1;
+            int m;
+            int true_target = target-numbers[i];
+            while (l <= r) {
+                m = (l + r) / 2;
+                // std::cout << "numbers[m]: " << numbers[m] << "   " << "true_target: " << true_target
+                //           << std::endl;
+                if (numbers[m] == true_target) {
+                    return {std::min(m + 1, i + 1), std::max(m + 1, i + 1)};
+                } else if (numbers[m] < true_target) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+        return {0, 1};
+    }
+};
