0f21ebfe8079018b1f56b9af6aec493894c66397
Abdul Amaan <110830860+Amaan1234567@users.noreply.github.com>
2026-09-02 22:20:40 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,26 @@
+#pragma GCC optimize("O3","unroll-loops");
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+            if (c == '(') {
+                st.push(')');
+            } else if (c == '[') {
+                st.push(']');
+            } else if (c == '{') {
+                st.push('}');
+            } else {
+                if (st.empty() || st.top() != c) {
+                    return false;
+                }
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
