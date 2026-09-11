192aa25aaf724edaad0c3d1a4fbf19e6a04ee826
Abdul Amaan <110830860+Amaan1234567@users.noreply.github.com>
2026-09-11 11:34:54 +0530

    Add: eating-bananas - submission-9

---
+++ Data Structures & Algorithms/eating-bananas/submission-9.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1;
+        int r = INT_MIN;
+        for(auto ele:piles){
+            l = min(l,ele);
+            r = max(r,ele);
+        }
+        int k;
+        int hours;
+        while(l<=r){
+
+            vector<int> piles_c = piles;
+            hours = h;
+            k = l +(r-l)/2;
+            for(auto ele:piles_c){
+                // std::cout<<"calc: "<<std::ceil((double)ele/(double)k)<<std::endl;
+                int res;
+                if(ele/k>0 && ele%k!=0){
+                    res = ele/k +1;
+                } else {
+                    res = ele/k;
+                }
+                hours -= max(res,1);
+            }
+            // std::cout<<"hours: "<<hours<<",k: "<<k<<std::endl;
+            if(hours < 0){
+                l = k+1;
+            } else if(hours == 0) {
+                r = k-1;
+            } else if(hours > 0){
+                r = k-1;
+            }
+        }
+        if(hours <0){
+            return k+1;
+        }else{
+            return k;
+        }
+    }
+};
